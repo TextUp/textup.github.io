b4b6aa3cf93c2ba7a49da4dafe75f42ab8698c01
Eric Bai <eric@ericbai.com>
2018-09-04 23:57:23 -0400

    initial pass at landing page redesign

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -1,9 +1,19 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var teamMembers = document.getElementsByClassName("team-member"),
-    numTeam = teamMembers.length;
-  for (var i = 0; i < numTeam; ++i) {
-    teamMembers[i].addEventListener("click", function() {
-      this.classList.toggle("team-member--open");
+$(document).ready(function() {
+  $(".will-call-slick")
+    .slick()
+    .on("beforeChange", function(event, slick, currentSlide, nextSlide) {
+      var sliderId = slick.$slider.attr("id");
+      $("input[data-controls-slick=" + sliderId + "]")
+        .prop("checked", false)
+        .filter("[data-slick-slide=" + nextSlide + "]")
+        .prop("checked", true);
     });
-  }
+  $("[data-controls-slick]").on("click", function() {
+    var slickId = this.dataset.controlsSlick,
+      slideIndex = this.dataset.slickSlide;
+    if (!slickId || !slideIndex) {
+      return;
+    }
+    $("#" + slickId).slick("slickGoTo", slideIndex);
+  });
 });
