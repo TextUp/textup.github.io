248ecda8e46fa3a5a7d7a7be37332fe8bca863d1
Eric Bai <eric@ericbai.com>
2018-12-21 09:09:59 -0500

    updated minimal-mistakes theme to 4.14.1, added invisible recaptcha to contact form

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -1,3 +1,12 @@
+---
+# Need frontmatter so we can access Jekyll variables here
+---
+
+var CONTACT_FORM_SELECTOR = "#textup-landing-contact-form",
+  CONTACT_FORM_PENDING_SELECTOR = "#textup-landing-contact-form-pending",
+  CONTACT_FORM_SUCCESS_SELECTOR = "#textup-landing-contact-form-success",
+  CONTACT_FORM_SPAM_SELECTOR = "#textup-landing-contact-form-spam";
+
 $(document).ready(function() {
   // Navigation
   // ----------
@@ -39,28 +48,14 @@ $(document).ready(function() {
     $("#" + slickId).slick("slickGoTo", slideIndex);
   });
 
-  // Team bios
-  // ---------
-
-  $(".team__headshot").on("click", function() {
-    $(this).toggleClass("team__headshot--show-details");
-  });
-
   // Contact form
   // ------------
 
-  $("#textup-landing-contact-form").on("submit", function(event) {
-    var honeypotVal = $("#textup-landing-contact-form .form__honeypot").val();
-    if (honeypotVal) {
-      // if honeypot was filled out, very likely spam so prevent form from being submitted
-      event.preventDefault();
-    } else {
-      $(this).fadeOut(function() {
-        $("#textup-landing-form-submit").toggleClass("hidden");
-      });
-    }
-  });
+  contactFormInit();
 });
+
+// Utility functions
+// -----------------
 
 function checkScrollFn() {
   var $window = $(window),
@@ -86,4 +81,69 @@ function debounce(func, wait) {
     clearTimeout(timeout);
     setTimeout(laterFn, wait);
   };
+}
+
+// Contact form captcha
+// --------------------
+
+function startContactFormSubmit(event) {
+  event.preventDefault();
+  contactFormPending();
+  // if honeypot was filled out, very likely spam so prevent form from being submitted
+  if ($(CONTACT_FORM_SELECTOR + " .form__honeypot").val()) {
+    contactFormSpam();
+  } else if (grecaptcha) {
+    grecaptcha.execute();
+  }
+}
+function contactFormVerifyHuman(responseKey) {
+  $.ajax({
+    method: "POST",
+    url: "{{ site.env.RECAPTCHA_TEST_ENDPOINT }}",
+    data: JSON.stringify({ key: responseKey }),
+    contentType: "application/json"
+  }).then(submitContactForm, contactFormSpam);
+}
+function submitContactForm() {
+  contactFormSuccess();
+  $(CONTACT_FORM_SELECTOR).submit();
+}
+
+// Contact form states
+// -------------------
+
+function contactFormInit() {
+  $(CONTACT_FORM_SELECTOR)
+    // only want once so that we don't keep on short circuiting when we programmatically submit
+    .one("submit", startContactFormSubmit)
+    .removeClass("hidden");
+  $(CONTACT_FORM_PENDING_SELECTOR).addClass("hidden");
+  $(CONTACT_FORM_SUCCESS_SELECTOR).addClass("hidden");
+  $(CONTACT_FORM_SPAM_SELECTOR).addClass("hidden");
+  setTimeout(function() {
+    if (grecaptcha) {
+      grecaptcha.reset();
+    }
+  }, 500);
+}
+
+function contactFormPending() {
+  $(CONTACT_FORM_SELECTOR).addClass("hidden");
+  $(CONTACT_FORM_PENDING_SELECTOR).removeClass("hidden");
+  $(CONTACT_FORM_SUCCESS_SELECTOR).addClass("hidden");
+  $(CONTACT_FORM_SPAM_SELECTOR).addClass("hidden");
+}
+
+function contactFormSuccess() {
+  $(CONTACT_FORM_SELECTOR).addClass("hidden");
+  $(CONTACT_FORM_PENDING_SELECTOR).addClass("hidden");
+  $(CONTACT_FORM_SUCCESS_SELECTOR).removeClass("hidden");
+  $(CONTACT_FORM_SPAM_SELECTOR).addClass("hidden");
+}
+
+function contactFormSpam() {
+  $(CONTACT_FORM_SELECTOR).addClass("hidden");
+  $(CONTACT_FORM_PENDING_SELECTOR).addClass("hidden");
+  $(CONTACT_FORM_SUCCESS_SELECTOR).addClass("hidden");
+  $(CONTACT_FORM_SPAM_SELECTOR).removeClass("hidden");
 }
